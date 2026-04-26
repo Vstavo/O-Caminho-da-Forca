2ff52b69e9693c961c34191ee6eb4bc59967e64d
Vstavo <gustavo.vsantana@sptech.school>
2026-04-25 23:32:27 -0300

    Fazendo a progress bar

--- frontend/js/pages/forca.js
+++ frontend/js/pages/forca.js
@@ -1,7 +1,20 @@
 export function paginaForca(main) {
     console.log('Página carregada');
-    main.className = ''
-    main.classList.add('dash-geral')
+    main.innerHTML = '';
+    main.className = '';
+    main.classList.add('dash-geral');
 
-    
+    main.innerHTML = `
+        <div class="conteudo">
+            <div class="cabecalhos">
+                <h1>Nível de força: <span id="nivel-de-forca">0</span></h1>
+                <h2>Sequência: <span id="sequencia">0</span><span> dias</span></h2>
+            </div>
+            <div class="corpo">
+                <div class="progress-bar">
+                    <canvas id="progress-bar-canvas" class="progress-bar-canvas" width="660" height="140"></canvas>
+                </div>
+            </div>
+        </div>
+    `;
 }
