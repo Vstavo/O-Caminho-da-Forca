f2ae41b181813777d121525078434079febf747b
Gustavo Vieira Santana <vieira.gusan@proton.me>
2026-05-03 01:16:52 -0300

    script do banco

--- backend/src/database/migration.sql
+++ backend/src/database/migration.sql
@@ -10,6 +10,8 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+insert into users values (default, 'Gustavo', 'vieira.gusan@gmail.com', 'Gustavo@IamEZ123', default);
+
 CREATE TABLE streaks (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT UNIQUE,
@@ -25,7 +27,7 @@ CREATE TABLE demon_logs (
     user_id INT,
     demon VARCHAR(50),
     log_date DATE,
-    status ENUM('resisted', 'failed', 'skipped') NOT NULL,
+    status ENUM('resistiu', 'falhou', 'pulou') NOT NULL,
     xp INT,
 
     UNIQUE KEY unique_log (user_id, demon, log_date),
@@ -37,8 +39,8 @@ CREATE TABLE mental_logs (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     log_date DATE,
-    state ENUM('focused', 'distracted', 'anxious', 'relaxed'),
-    blocker ENUM('fear', 'insecurity', 'laziness', 'distraction'),
+    state ENUM('focado', 'distraido', 'ansioso', 'relaxado'),
+    blocker ENUM('medo', 'inseguranca', 'preguica', 'distracao', 'luxuria'),
 
     UNIQUE KEY unique_log (user_id, log_date),
 
@@ -51,7 +53,7 @@ CREATE TABLE goals (
     title VARCHAR(150),
     description TEXT,
     progress INT DEFAULT 0,
-    status ENUM('active', 'completed') DEFAULT 'active',
+    status ENUM('ativo', 'completo') DEFAULT 'ativo',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
@@ -67,7 +69,39 @@ CREATE TABLE xp_logs (
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-SELECT * FROM demon_logs;
-SELECT * FROM mental_logs;
-SELECT * FROM goals;
-SELECT * FROM xp_logs;
+-- SELECT * FROM demon_logs;
+-- SELECT * FROM mental_logs;
+-- SELECT * FROM xp_logs;
+
+insert into mental_logs (user_id, state, blocker, log_date) values ( 1, 'relaxado', null, '2026-04-24');
+insert into mental_logs (user_id, state, blocker, log_date) values ( 1, 'relaxado', null, '2026-04-25');
+insert into mental_logs (user_id, state, blocker, log_date) values ( 1, 'focado', null, '2026-04-26');
+insert into mental_logs (user_id, state, blocker, log_date) values ( 1, 'ansioso', 'luxuria', '2026-04-27');
+insert into mental_logs (user_id, state, blocker, log_date) values ( 1, 'ansioso', 'luxuria', '2026-04-28');
+insert into mental_logs (user_id, state, blocker, log_date) values ( 1, 'ansioso', 'preguica', '2026-04-29');
+insert into mental_logs (user_id, state, blocker, log_date) values ( 1, 'distraido', 'distracao', '2026-04-30');
+insert into xp_logs (user_id, amount, source) values ( 1, 10, 'mental');
+insert into xp_logs (user_id, amount, source) values ( 1, 10, 'mental');
+insert into xp_logs (user_id, amount, source) values ( 1, 10, 'mental');
+insert into xp_logs (user_id, amount, source) values ( 1, 10, 'mental');
+insert into xp_logs (user_id, amount, source) values ( 1, 10, 'mental');
+insert into xp_logs (user_id, amount, source) values ( 1, 10, 'mental');
+insert into xp_logs (user_id, amount, source) values ( 1, 10, 'mental');
+
+-- SELECT blocker, COUNT(blocker) as quantidade_blocker FROM mental_logs WHERE user_id = 1 AND blocker IS NOT NULL AND log_date >= CURDATE() - INTERVAL 7 DAY GROUP BY blocker ORDER BY quantidade_blocker DESC;
+-- SELECT state, COUNT(state) as quantidade_state FROM mental_logs WHERE user_id = 1 AND log_date >= CURDATE() - INTERVAL 7 DAY GROUP BY state ORDER BY quantidade_state DESC LIMIT 1;
+
+/* 
+SELECT state, log_date 
+FROM mental_logs 
+WHERE user_id = 1
+ORDER BY log_date DESC;
+*/
+
+-- SELECT (SELECT state FROM mental_logs WHERE user_id = 1 ORDER BY log_date DESC LIMIT 1) as state, (SELECT blocker FROM mental_logs WHERE user_id = 1 ORDER BY log_date DESC LIMIT 1) as blocker, (SELECT COUNT(*) FROM mental_logs WHERE user_id = 1 AND log_date >= CURDATE() - INTERVAL 7 DAY) as consistencia;
+
+-- SELECT PARA CALCULAR XP
+
+-- SELECT SUM(amount) FROM xp_logs WHERE user_id = ?;
+
+-- drop database caminhodaforca;
